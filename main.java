878cc5c96d9a63e4341faa4268b9a21871677449
Yanjie-GH <yanjie.hi@gmail.com>
2020-07-03 21:28:36 -0700

    Expression Assignment Hand-in

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String args[]){
+        System.out.println( Math.pow( Math.pow(1024,0.1) ,6) );
+
+        System.out.println( (1.5 * 10 * 2 + 2) * (Math.cos(0) + Math.pow(233,0)) );
+
+        System.out.println( (867 / 3 - 597 + 962 - 14) / 10.0 );
+
+        System.out.println( Math.pow(6,4) * 4 / 81 );
+
+        System.out.println( 4289.0 % 65 );
+    }
+}
